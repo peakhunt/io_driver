7fea48de39e57b17bf2a4565ee01eb5bd0539617
Hawk Hyouck Kim <hawk.kim1974@gmail.com>
2018-08-30 17:16:14 +0900

    added convenience inlines

--- src/io_timer.h
+++ src/io_timer.h
@@ -15,4 +15,23 @@ typedef struct
 extern void io_timer_init(io_driver_t* driver, io_timer_t* t, int tickrate);
 extern void io_timer_deinit(io_timer_t* t);
 
+static inline void
+io_timer_start(io_timer_t* t, SoftTimerElem* e, int expires)
+{
+  soft_timer_add(&t->st, e, expires);
+}
+
+static inline void
+io_timer_stop(io_timer_t* t, SoftTimerElem* e)
+{
+  soft_timer_del(&t->st, e);
+}
+
+static inline void
+io_timer_restart(io_timer_t* t, SoftTimerElem* e, int expires)
+{
+  io_timer_stop(t, e);
+  io_timer_start(t, e, expires);
+}
+
 #endif /* !__IO_TIMER_DEF_H__ */
