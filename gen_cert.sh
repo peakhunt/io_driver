cd384dcc8be30cc2b4218e960846ea7bcf8cfdd1
Hawk Hyouck Kim <hawk.kim1974@gmail.com>
2018-09-06 16:09:58 +0900

    adding cert gen script

---
+++ gen_cert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# generate a private key
+#
+openssl genrsa -out server.pem 2048
+
+#
+# generate CSR(Certificate Signing Request)
+#
+openssl req -new -key server.pem -out server.csr
+
+#
+# generate self signed cert
+#
+openssl x509 -req -days 365 -in server.csr -signkey server.pem -out server.crt
